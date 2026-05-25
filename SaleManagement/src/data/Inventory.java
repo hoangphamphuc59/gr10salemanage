bbc5387c9e24559e15779f3b3ae24525e93a8029
hoangphamphuc59 <136866315+hoangphamphuc59@users.noreply.github.com>
2026-05-25 18:57:53 +0700

    getter, setter, methods

---
+++ SaleManagement/src/data/Inventory.java
@@ -0,0 +1,11 @@
+package data;
+
+public class Inventory {
+
+    public Inventory() {
+    }
+    
+    public int updateProductStock(Transaction t){
+        return -1;
+    }
+}
---
+++ SaleManagement/src/salemanagement/SaleManagement.java
@@ -0,0 +1,9 @@
+package salemanagement;
+
+public class SaleManagement {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
