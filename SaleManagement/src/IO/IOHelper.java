d90971825c37e785855a315bb7f88b6a85792282
hoangphamphuc59 <136866315+hoangphamphuc59@users.noreply.github.com>
2026-06-11 11:16:01 +0700

    IOhelper LocalDate
    import java.time.localdate into IOhelper

--- SaleManagement/src/IO/IOHelper.java
+++ SaleManagement/src/IO/IOHelper.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.time.LocalDate;
 
 public class IOHelper {
 
@@ -241,7 +242,7 @@ public class IOHelper {
                             i += 6;
                         }
 
-                        java.time.LocalDate localDate = java.time.LocalDate.parse(dateStr);
+                        LocalDate localDate = LocalDate.parse(dateStr);
                         Transaction transaction = new Transaction(transId, customer, localDate, totalAmount, status, items);
                         traList.add(transaction);
 
